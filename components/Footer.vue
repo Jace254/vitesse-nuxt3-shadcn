435e888bc26bb5ada4f95464469433ad9f5d1627
Hypnos <47209391+Jace254@users.noreply.github.com>
2023-12-18 00:01:08 +0300

    chore: update github link on Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div text="xl gray4" m-5 flex="~ gap3" justify-center>
     <NuxtLink i-carbon-campsite to="/" />
-    <a i-carbon-logo-github href="https://github.com/antfu/vitesse-nuxt3" target="_blank" />
+    <a i-carbon-logo-github href="https://github.com/Jace254/vitesse-nuxt3-shadcn" target="_blank" />
     <DarkToggle />
   </div>
 </template>
